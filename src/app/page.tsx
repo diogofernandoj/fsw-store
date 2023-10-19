de35a5d16aa05200676bfc0f4a4b658d76e7f5cb
Diogo Jorge <diogofernandoj@gmail.com>
2023-10-18 21:42:25 -0300

    feat: add auth handler to header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 const Home = () => {
+  const { data } = useSession();
   return (
     <div>
-      <h1>Home page</h1>
+      <h1>{data?.user?.name}</h1>
     </div>
   );
 };
