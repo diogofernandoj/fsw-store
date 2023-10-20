605712e907d516dbfaf4f65e9945bef7789fb8f8
Diogo Jorge <diogofernandoj@gmail.com>
2023-10-19 21:20:29 -0300

    feat: add calcProductTotalPrice helper function

---
+++ src/helpers/product.ts
@@ -0,0 +1,26 @@
+import { Product } from "@prisma/client";
+
+export interface ProductWithTotalPrice extends Product {
+  totalPrice: number;
+}
+
+export const calcProductTotalPrice = (
+  product: Product,
+): ProductWithTotalPrice => {
+  if (product.discountPercentage === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+
+  const discountPrice =
+    Number(product.basePrice) * (product.discountPercentage / 100);
+
+  const totalPrice = Number(product.basePrice) - discountPrice;
+
+  return {
+    ...product,
+    totalPrice,
+  };
+};
