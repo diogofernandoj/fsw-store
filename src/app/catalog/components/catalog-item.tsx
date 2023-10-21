5fae6fa345793f1dbdaddfbdd7512873ad84e591
Diogo Jorge <diogofernandoj@gmail.com>
2023-10-20 21:39:56 -0300

    feat: add CategoryProducts page link to CatalogItem

--- src/app/catalog/components/catalog-item.tsx
+++ src/app/catalog/components/catalog-item.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Category } from "@prisma/client";
+import Link from "next/link";
 
 interface CatalogItemProps {
   category: Category;
@@ -7,21 +8,23 @@ interface CatalogItemProps {
 
 const CatalogItem = ({ category }: CatalogItemProps) => {
   return (
-    <div className="flex flex-col">
-      <div className="flex h-[140px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-gradient-to-r from-[#5033C3] to-[#5033c333]">
-        <Image
-          src={category.imageUrl}
-          alt={category.name}
-          width={0}
-          height={0}
-          sizes="100vw"
-          className="h-auto w-1/2 object-contain"
-        />
+    <Link href={`/category/${category.slug}`}>
+      <div className="flex flex-col">
+        <div className="flex h-[140px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-gradient-to-r from-[#5033C3] to-[#5033c333]">
+          <Image
+            src={category.imageUrl}
+            alt={category.name}
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-1/2 object-contain"
+          />
+        </div>
+        <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
+          <p className="text-center font-semibold">{category.name}</p>
+        </div>
       </div>
-      <div className="rounded-bl-lg rounded-br-lg bg-accent py-3">
-        <p className="text-center font-semibold">{category.name}</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
