896bc334879e9d256c960458c0b5c47be6700c5c
Diogo Jorge <diogofernandoj@gmail.com>
2023-10-29 07:39:38 -0300

    fix: ensure cart has the necessary width for its content

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -125,7 +125,7 @@ const Header = () => {
           </Button>
         </SheetTrigger>
 
-        <SheetContent>
+        <SheetContent className="min-w-[340px]">
           <Cart />
         </SheetContent>
       </Sheet>
