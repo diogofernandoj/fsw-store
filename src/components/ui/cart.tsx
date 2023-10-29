b9e2e949d05b981c2efe14655d4e63035377c8b1
Diogo Jorge <diogofernandoj@gmail.com>
2023-10-29 07:38:52 -0300

    feat: show empty cart image when cart is empty

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,6 +5,7 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
+import Image from "next/image";
 
 const Cart = () => {
   const { cartProducts } = useContext(CartContext);
@@ -19,11 +20,27 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-4">
-        {cartProducts.map((product) => (
-          <CartItem key={product.id} product={product} />
-        ))}
-      </div>
+      {!cartProducts.length ? (
+        <div className="flex h-full w-full flex-col gap-6">
+          <p className="text-center text-xl font-bold">
+            Você não adicionou itens ao carrinho!
+          </p>
+          <Image
+            src="/empty-cart.svg"
+            alt="Empty cart"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="mx-auto h-[65%] w-auto max-w-[85%]"
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {cartProducts.map((product) => (
+            <CartItem key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
