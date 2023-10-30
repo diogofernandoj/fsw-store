5ee178c81c58535978c6c92d2ce8a236e5dcbb35
Diogo Jorge <diogofernandoj@gmail.com>
2023-10-30 19:21:54 -0300

    fix: ensure payment success route is in the right directory

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -24,6 +24,8 @@ export const POST = async (request: Request) => {
       },
     );
     const lineItems = sessionWithLineItems.line_items;
+
+    console.log(lineItems);
   }
 
   return NextResponse.json({ received: true });
