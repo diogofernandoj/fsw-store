1be2f59ab92f656a0d051b3a33339f7442e6da82
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-07 12:16:32 -0300

    feat: ensure product item is responsive on desktop

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -11,7 +11,7 @@ const ProductItem = ({ product }: ProductItemProps) => {
   return (
     <Link href={`/product/${product.slug}`}>
       <div className="flex w-full min-w-[160px] flex-col gap-2">
-        <div className="relative flex h-40 w-full items-center justify-center rounded-lg bg-accent">
+        <div className="relative flex h-40 w-full items-center justify-center rounded-lg bg-accent lg:h-[200px] lg:w-[200px]">
           {product.discountPercentage > 0 && (
             <DiscountBadge className="absolute left-2 top-2">
               {product.discountPercentage}
@@ -29,21 +29,21 @@ const ProductItem = ({ product }: ProductItemProps) => {
         </div>
 
         <div className="ml-2 flex flex-col">
-          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm lg:text-base">
             {product.name}
           </p>
           <div className="flex items-center gap-1">
             {product.discountPercentage > 0 ? (
               <>
-                <p className="text-sm font-semibold">
+                <p className="text-sm font-semibold lg:text-base">
                   {currencyNumber(product.totalPrice)}
                 </p>
-                <p className="text-xs line-through opacity-75">
+                <p className="text-xs line-through opacity-75 lg:text-sm">
                   {currencyNumber(product.basePrice.toNumber())}
                 </p>
               </>
             ) : (
-              <p className="text-sm font-semibold">
+              <p className="text-sm font-semibold lg:text-base">
                 {currencyNumber(product.basePrice.toNumber())}
               </p>
             )}
--- src/components/ui/section-title.tsx
+++ src/components/ui/section-title.tsx
@@ -2,7 +2,7 @@ import { ComponentProps } from "react";
 
 const SectionTitle = ({ children, ...props }: ComponentProps<"p">) => {
   return (
-    <p className="mb-2 ml-5 font-bold uppercase" {...props}>
+    <p className="mb-2 ml-5 font-bold uppercase lg:text-xl" {...props}>
       {children}
     </p>
   );
