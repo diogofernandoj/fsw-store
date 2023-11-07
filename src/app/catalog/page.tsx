bdd49dcbc2f5138ad9f05deec005e936b77f2439
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-07 15:17:57 -0300

    feat: ensure Catalog page is responsive on desktop

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -7,13 +7,13 @@ const CatalogPage = async () => {
   const categories = await prismaClient.category.findMany({});
 
   return (
-    <div className="flex flex-col gap-8 px-4">
+    <div className="mx-auto flex max-w-[1400px] flex-col gap-8 px-4 lg:px-20">
       <Badge variant="heading">
         <LayoutDashboardIcon size={16} />
         Catálogo
       </Badge>
 
-      <div className="grid grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8 lg:grid-cols-3">
         {categories.map((category) => (
           <CatalogItem key={category.id} category={category} />
         ))}
