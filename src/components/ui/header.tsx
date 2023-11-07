8c39904443592d0159866f49a437fc318a3a1e5c
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-07 16:42:06 -0300

    feat: ensure home button on header menu redirects user to home page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -90,9 +90,16 @@ const Header = () => {
               </Button>
             )}
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <HomeIcon size={16} /> Início
-            </Button>
+            <SheetClose asChild>
+              <Link href="/">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <HomeIcon size={16} /> Início
+                </Button>
+              </Link>
+            </SheetClose>
 
             <SheetClose asChild>
               <Link href="/offers">
