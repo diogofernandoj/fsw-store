ba091caaf057bc5ae40d7963b91b9b470772abe0
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-07 08:25:07 -0300

    feat: add badge title to OffersPage

--- src/app/offers/page.tsx
+++ src/app/offers/page.tsx
@@ -1,6 +1,8 @@
+import { Badge } from "@/components/ui/badge";
 import ProductItem from "@/components/ui/product-item";
 import { calcProductTotalPrice } from "@/helpers/product";
 import { prismaClient } from "@/lib/prisma";
+import { PercentIcon } from "lucide-react";
 
 const OffersPage = async () => {
   const offers = await prismaClient.product.findMany({
@@ -12,13 +14,22 @@ const OffersPage = async () => {
   });
 
   return (
-    <div className="grid grid-cols-2 gap-8 px-5">
-      {offers.map((product) => (
-        <ProductItem
-          key={product.id}
-          product={calcProductTotalPrice(product)}
-        />
-      ))}
+    <div className="flex flex-col px-5">
+      <Badge
+        variant="outline"
+        className="mb-8 flex w-fit items-center gap-1 border-2 border-primary px-3 py-1 text-base uppercase"
+      >
+        <PercentIcon />
+        Ofertas
+      </Badge>
+      <div className="grid grid-cols-2 gap-8">
+        {offers.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={calcProductTotalPrice(product)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
