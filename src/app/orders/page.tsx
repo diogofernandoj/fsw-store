1f465cdb4807dcda117b3db030059a3fee3b34f6
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-11 13:43:07 -0300

    feat: add OrdersPage

---
+++ src/app/orders/page.tsx
@@ -0,0 +1,44 @@
+import { Badge } from "@/components/ui/badge";
+import { authOptions } from "@/lib/auth";
+import { prismaClient } from "@/lib/prisma";
+import { PackageSearch } from "lucide-react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import OrderItem from "./components/order-item";
+
+const OrdersPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
+
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: session.user.id,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div>
+      <Badge variant="heading">
+        <PackageSearch /> Meus pedidos
+      </Badge>
+
+      <div className="mt-5 flex flex-col gap-5">
+        {orders.map((order) => (
+          <OrderItem key={order.id} order={order} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OrdersPage;
