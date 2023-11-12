8b1b18cd8b0f996cfd4d2bfa06b5977418832cc9
Diogo Jorge <diogofernandoj@gmail.com>
2023-11-12 16:41:30 -0300

    feat: ensure header is responsive for desktop

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -9,6 +9,8 @@ import {
   ListOrderedIcon,
   LogOutIcon,
   PackageSearch,
+  SearchIcon,
+  User2Icon,
 } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
@@ -27,6 +29,14 @@ import Cart from "./cart";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
 
 const Header = () => {
   const { status, data } = useSession();
@@ -44,7 +54,7 @@ const Header = () => {
   return (
     <Card className="flex items-center justify-between p-5">
       <Sheet>
-        <SheetTrigger asChild>
+        <SheetTrigger asChild className="lg:hidden">
           <Button variant="outline" size="icon">
             <MenuIcon />
           </Button>
@@ -56,7 +66,7 @@ const Header = () => {
 
           {data?.user && (
             <div className="flex flex-col">
-              <div className=" flex items-center gap-2 py-4">
+              <div className="flex items-center gap-2 py-4">
                 <Avatar>
                   <AvatarFallback>
                     {data.user.name?.[0].toUpperCase()}
@@ -146,26 +156,116 @@ const Header = () => {
 
       <Link href="/">
         <h1 className="text-lg font-semibold">
-          <span className="text-primary">Jota</span> Store
+          <span className="text-primary">Jota</span>Store
         </h1>
       </Link>
 
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="relative">
-            {cartProducts.length > 0 && (
-              <Badge className="absolute -right-2 -top-2 flex h-5 w-6 items-center justify-center p-1 text-xs">
-                {cartProducts.length}
-              </Badge>
-            )}
-            <ShoppingCartIcon />
+      <div className="hidden items-center gap-2 lg:flex">
+        <Link href="/">
+          <Button variant="ghost" className="text-sm opacity-75">
+            Início
           </Button>
-        </SheetTrigger>
+        </Link>
+        <Link href="/catalog">
+          <Button variant="ghost" className="text-sm opacity-75">
+            Catálogo
+          </Button>
+        </Link>
+        <Link href="/offers">
+          <Button variant="ghost" className="text-sm opacity-75">
+            Ofertas
+          </Button>
+        </Link>
+      </div>
 
-        <SheetContent className="min-w-[340px] lg:max-w-[550px]">
-          <Cart />
-        </SheetContent>
-      </Sheet>
+      <div className="flex items-center gap-2">
+        <Button
+          variant="outline"
+          size="icon"
+          className="hidden items-center justify-center lg:flex"
+        >
+          <SearchIcon />
+        </Button>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              className="hidden items-center justify-center lg:flex"
+              variant="outline"
+              size="icon"
+            >
+              <User2Icon />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent>
+            {data?.user && (
+              <>
+                <DropdownMenuLabel>
+                  <div className="flex flex-col">
+                    <div className="flex items-center gap-2 py-4">
+                      <Avatar className="h-8 w-8">
+                        <AvatarFallback>
+                          {data.user.name?.[0].toUpperCase()}
+                        </AvatarFallback>
+
+                        {data.user.image && (
+                          <AvatarImage src={data.user.image} />
+                        )}
+                      </Avatar>
+
+                      <div className="flex flex-col">
+                        <p className="text-sm font-medium">{data.user.name}</p>
+                        <p className="text-xs opacity-75">Boas compras!</p>
+                      </div>
+                    </div>
+                  </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+              </>
+            )}
+            {status === "unauthenticated" ? (
+              <DropdownMenuItem
+                onClick={handleLoginClick}
+                className="flex items-center gap-2"
+              >
+                <LogInIcon size={16} /> Fazer login
+              </DropdownMenuItem>
+            ) : (
+              <DropdownMenuItem
+                onClick={handleLogoutClick}
+                className="flex items-center gap-2"
+              >
+                <LogOutIcon size={16} /> Fazer logout
+              </DropdownMenuItem>
+            )}
+            {data?.user && (
+              <Link href="/orders">
+                <DropdownMenuItem className="flex items-center gap-2">
+                  <PackageSearch size={16} /> Meus pedidos
+                </DropdownMenuItem>
+              </Link>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="relative">
+              {cartProducts.length > 0 && (
+                <Badge className="absolute -right-2 -top-2 flex h-5 w-6 items-center justify-center p-1 text-xs">
+                  {cartProducts.length}
+                </Badge>
+              )}
+              <ShoppingCartIcon />
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent className="min-w-[340px] lg:max-w-[550px]">
+            <Cart />
+          </SheetContent>
+        </Sheet>
+      </div>
     </Card>
   );
 };
